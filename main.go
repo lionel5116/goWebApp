b911040d4016188475cda89eb96cc84e1093eb0f
lioneljones <lioneljones5116@gmail.com>
2021-10-02 13:45:25 -0500

    more stuff

--- main.go
+++ main.go
@@ -1,15 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	html := "<strong>I am Rick James Bitch !!!!</strong>"
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, html)
+	/*
+		html := "<strong>I am Rick James Bitch !!!!</strong>"
+		w.Header().Set("Content-Type", "text/html")
+		fmt.Fprint(w, html)
+	*/
+	t, err := template.ParseFiles("index.html")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	err = t.Execute(w, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
 }
 
 func main() {
